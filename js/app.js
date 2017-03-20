c3ca1202f0fcfcb0f12e7c5f21dbefd7c3296ed9
chapulina <burajiru.no.chapulina@gmail.com>
2017-03-19 18:14:51 -0700

    flyover labs, gdg, readwrite

--- js/app.js
+++ js/app.js
@@ -31,9 +31,9 @@ $(document).ready(function () {
   // change is-checked class on buttons
   $('.btn-group').each(function(i, buttonGroup) {
     var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', 'btn', function() {
+    $buttonGroup.on('click', 'button', function() {
       $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $( this ).addClass('is-checked');
+      $(this).addClass('is-checked');
     });
   });
 
