190d78d835ad0c106d5becce65699a6542f98bdc
chapulina <burajiru.no.chapulina@gmail.com>
2017-04-09 11:21:09 -0700

    show hover effect for item in the center of screen, for mobile

--- js/app.js
+++ js/app.js
@@ -1,14 +1,51 @@
-var filter = [];
+let filter = [];
+
+// Only used by touch devices
+let $window;
+let $gridItems;
+
+// Used by scrolling on touch devices
+function check_if_in_view() {
+
+  let windowHeight = $window.height();
+
+  // Boundaries of central view area
+  let viewCenter = $window.scrollTop() + windowHeight*0.5;
+
+  // Iterate over items
+  $.each($gridItems, function() {
+    let $element = $(this);
+    let elementHeight = $element.outerHeight();
+    let elementTop = $element.offset().top;
+    let elementBottom = (elementTop + elementHeight);
+
+    // Mark whether item is within the center view
+    if ((elementBottom >= viewCenter) &&
+        (elementTop <= viewCenter)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+// first time user touches the screen
+document.addEventListener('touchstart', function addtouchclass(e){
+
+  // add "can-touch" class to document root using classList API
+  document.documentElement.classList.add('can-touch')
+
+  // Cache window and grid items for scrolling effect on touch devices
+  $gridItems = $('.grid-item');
+  $window = $(window);
+  $window.on('scroll resize', check_if_in_view);
+  $window.trigger('scroll');
+
+  // de-register touchstart event
+  document.removeEventListener('touchstart', addtouchclass, false)
+}, false)
 
 $(document).ready(function () {
-
-  // first time user touches the screen
-  document.addEventListener('touchstart', function addtouchclass(e){
-    // add "can-touch" class to document root using classList API
-    document.documentElement.classList.add('can-touch')
-    // de-register touchstart event
-    document.removeEventListener('touchstart', addtouchclass, false)
-  }, false)
 
   // Init Isotope
   let $grid = $('.grid').isotope({
